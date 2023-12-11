dc401e2c34cd6fca4c6c275f2b96308ff60b6795
skyDe <sky_de1991@outlook.com>
2023-12-11 06:32:27 -0800

    fix for nav

---
+++ src/hooks/useVeiwPort.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+
+
+export const useVeiwPort = ({breakPoint}) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+      const handleResize = () => {
+        const newWidth = window.innerWidth;
+        setWindowWidth(newWidth);
+
+        if (newWidth > breakPoint) {
+          setIsOpen(true);
+        } else if (newWidth <= breakPoint) {
+          setIsOpen(false);
+        }
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      // Clean-up
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, [windowWidth]);
+    return { isOpen, setIsOpen }
+}
+
+
+
+
