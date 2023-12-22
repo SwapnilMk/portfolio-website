ca8cda00b3b100d7124956e38ad2ab8b54455fd2
SwapnilMk <mswapnil218@gmail.com>
2023-12-22 13:23:18 +0530

    Images URL updated !

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export const data = [
   {
     id: 1,
-    img: 'src/assets/Projects/Notes.png',
+    img: 'src/assets/Notes.png',
     title: "Quick Notes",
     desc: "Simple Notes taking application using Bootstrap, Tailwind and Venila Javascript !",
     link: "https://swapnilmk.github.io/Quick-notes/",
@@ -9,7 +9,7 @@ export const data = [
   },
   {
     id: 2,
-    img: 'src/assets/Projects/NewsHunt.png',
+    img: 'src/assets/NewsHunt.png',
     title: "News Hunt",
     desc: "Welcome to my news website, where you can find the latest updates on current events and important issues.",
     link: "https://news-huntweb.netlify.app/",
@@ -17,7 +17,7 @@ export const data = [
   },
   {
     id: 3,
-    img: 'src/assets/Projects/techtea.png',
+    img: 'src/assets/techtea.png',
     title: "TechTea",
     desc: "techtea is a React-based blog website that allows users to log in, create accounts, and share tech news :)",
     link: "https://techtea.netlify.app/",
