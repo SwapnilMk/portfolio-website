bbab22480533906364d649896d3d92fc97995086
SwapnilMk <mswapnil218@gmail.com>
2024-07-15 03:32:50 +0530

    testing aws deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      this is a homepage
+      this is a homepage testing deployement for aws next js
     </main>
   );
 }
