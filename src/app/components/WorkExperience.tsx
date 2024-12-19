c5bffb78c81cc9f0d4dcafaaefb4c409488bc89e
Swapnil Mahadik <mswapnil218@gmail.com>
2024-12-20 00:50:10 +0530

    initial commit for new updated Portfolio

--- src/app/components/WorkExperience.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WorkExperience = () => {
-  return (
-    <div>WorkExperience</div>
-  )
-}
-
-export default WorkExperience
--- src/app/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { expandableSlice, explorerSlice, sectionSlice, tabsSlice } from './slices';
-
-export const reducer = {
-  expandable: expandableSlice.reducer,
-  explorer: explorerSlice.reducer,
-  sections: sectionSlice.reducer,
-  tabs: tabsSlice.reducer,
-};
